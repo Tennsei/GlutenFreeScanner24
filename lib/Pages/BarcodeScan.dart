2c5cc0db8b5227e68675134e2d0a8a37cd7524a7
Arnav Juneja <arnavjuneja2007@gmail.com>
2024-03-17 16:29:57 -0700

    added flashlight functionality

--- lib/Pages/BarcodeScan.dart
+++ lib/Pages/BarcodeScan.dart
@@ -12,42 +12,79 @@ class ScanCode extends StatefulWidget {
 class _ScanCodeState extends State<ScanCode> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Scan')),
-      
-      body: MobileScanner(
-        controller: MobileScannerController(
-          detectionSpeed: DetectionSpeed.noDuplicates,
-          facing: CameraFacing.back,
-          torchEnabled: true,
-        ),
-        onDetect: (capture){
-          final List<Barcode> barcodes = capture.barcodes;
-          final Uint8List? image = capture.image;
-          for (final barcode in barcodes) {
-            debugPrint ('Barcode found! ${barcode.rawValue}');
-          }
-          if (image != null) {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: Text(
-                    barcodes.first.rawValue ?? "",
-                  ),
-                  content: Image(
-                    image: MemoryImage(image),
-                  ),
-                );
-              }
-            );
-            Future.delayed(const Duration(seconds: 5), () {
-              Navigator.pop(context);
-            });
-          }
-        },
-      )
+    MobileScannerController cameraController = MobileScannerController();
 
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Scanner'),
+          actions: [
+            IconButton(
+              color: Colors.white,
+              icon: ValueListenableBuilder(
+                valueListenable: cameraController.torchState,
+                builder: (context, state, child) {
+                  switch (state as TorchState) {
+                    case TorchState.off:
+                      return const Icon(Icons.flashlight_off,
+                          color: Colors.grey);
+                    case TorchState.on:
+                      return const Icon(Icons.flash_on, color: Colors.amber);
+                  }
+                },
+              ),
+              iconSize: 32.0,
+              onPressed: () => cameraController.toggleTorch(),
+            ),
+            IconButton(
+              color: Colors.white,
+              icon: ValueListenableBuilder(
+                valueListenable: cameraController.cameraFacingState,
+                builder: (context, state, child) {
+                  switch (state as CameraFacing) {
+                    case CameraFacing.front:
+                      return const Icon(Icons.camera_front);
+                    case CameraFacing.back:
+                      return const Icon(Icons.camera_rear);
+                  }
+                },
+              ),
+              iconSize: 32.0,
+              onPressed: () => cameraController.switchCamera(),
+            ),
+          ],
+        ),
+        body: MobileScanner(
+          controller: MobileScannerController(
+            detectionSpeed: DetectionSpeed.noDuplicates,
+            facing: CameraFacing.back,
+            torchEnabled: true,
+          ),
+          onDetect: (capture) {
+            final List<Barcode> barcodes = capture.barcodes;
+            final Uint8List? image = capture.image;
+            for (final barcode in barcodes) {
+              debugPrint('Barcode found! ${barcode.rawValue}');
+            }
+            if (image != null) {
+              showDialog(
+                context: context,
+                builder: (context) => //{
+                    // return AlertDialog(
+                    //   title: Text(
+                    //     barcodes.first.rawValue ?? "",
+                    //   ),
+                    //   content: Image(
+                    //     image: MemoryImage(image),
+                    //   ),
+                    // );
+                    Image(image: MemoryImage(image)),
+                //}
+              );
+              Future.delayed(const Duration(seconds: 5), () {
+                Navigator.pop(context);
+              });
+            }
+          },
+        ));
   }
 }
