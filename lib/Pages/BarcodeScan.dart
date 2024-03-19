1cab8e999c3edf01cda66f1012879f4d0e92ed0a
Arnav Juneja <arnavjuneja2007@gmail.com>
2024-03-18 22:39:21 -0700

    dfjkasompdsa

--- lib/Pages/BarcodeScan.dart
+++ lib/Pages/BarcodeScan.dart
@@ -1,68 +1,60 @@
 import 'dart:typed_data';
-import 'package:get/get.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 
-import 'package:test1/Pages/OpenFoodFactsAPi.dart';
-
 class ScanCode extends StatefulWidget {
-  const ScanCode({super.key});
+  const ScanCode({Key? key});
 
   @override
   State<ScanCode> createState() => _ScanCodeState();
 }
 
 class _ScanCodeState extends State<ScanCode> {
+  MobileScannerController cameraController = MobileScannerController();
+  bool isFlashlightOn = false;
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+
+  Future<void> toggleFlashlight() async {
+    // final TorchState torchState = await cameraController.toggleTorch();
+    // setState(() {
+    //   isFlashlightOn = torchState == TorchState.on;
+    // });
+  }
+
+  Future<void> switchCamera() async {
+    await cameraController.switchCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
-    MobileScannerController cameraController = MobileScannerController();
-    OpenFoodAPIConfiguration.userAgent = UserAgent(name: 'GF Scanner');
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Scanner'),
         actions: [
           IconButton(
-            color: Colors.white,
-            icon: ValueListenableBuilder(
-              valueListenable: cameraController.torchState,
-              builder: (context, state, child) {
-                switch (state as TorchState) {
-                  case TorchState.off:
-                    return const Icon(Icons.flash_off, color: Colors.grey);
-                  case TorchState.on:
-                    return const Icon(Icons.flash_on, color: Colors.yellow);
-                }
-              },
+            icon: Icon(
+              isFlashlightOn ? Icons.flash_on : Icons.flash_off,
+              color: Colors.white,
             ),
-            iconSize: 32.0,
-            onPressed: () => cameraController.toggleTorch(),
+            onPressed: toggleFlashlight,
           ),
           IconButton(
-            color: Colors.white,
-            icon: ValueListenableBuilder(
-              valueListenable: cameraController.cameraFacingState,
-              builder: (context, state, child) {
-                switch (state as CameraFacing) {
-                  case CameraFacing.front:
-                    return const Icon(Icons.camera_front);
-                  case CameraFacing.back:
-                    return const Icon(Icons.camera_rear);
-                }
-              },
+            icon: Icon(
+              Icons.switch_camera,
+              color: Colors.white,
             ),
-            iconSize: 32.0,
-            onPressed: () => cameraController.switchCamera(),
+            onPressed: switchCamera,
           ),
         ],
       ),
       body: MobileScanner(
-        controller: MobileScannerController(
-          detectionSpeed: DetectionSpeed.noDuplicates,
-          facing: CameraFacing.back,
-          torchEnabled: false,
-        ),
+        controller: cameraController,
         onDetect: (capture) async {
           final List<Barcode> barcodes = capture.barcodes;
           final Uint8List? image = capture.image;
@@ -72,32 +64,15 @@ class _ScanCodeState extends State<ScanCode> {
             if (barcodeValue != null) {
               print('Barcode value: $barcodeValue');
 
-              final ProductQueryConfiguration configuration =
-                  ProductQueryConfiguration(
-                barcodeValue,
-                version: ProductQueryVersion.v3,
-                language: OpenFoodFactsLanguage.ENGLISH,
-
-              );
-
               try {
                 final ProductResultV3 result =
-                    await OpenFoodAPIClient.getProductV3(configuration);
-
-                print('API response status: ${result.status}');
-                print('API response product: ${result.product}');
-                print('API response error: ${result.errors}');
-                print('API response error message: ${result.errors}');
+                    await OpenFoodAPIClient.getProductV3(barcodeValue as ProductQueryConfiguration);
 
                 if (result.status == 1 && result.product != null) {
                   final product = result.product!;
-                  final productName =
-                      product.getBestProductName(OpenFoodFactsLanguage.ENGLISH);
-                  // final energy = product.nutriments?.getEnergyValue();
-                  // final fat = product.nutriments?.getFat100g();
-                  // final carbohydrates =
-                  //     product.nutriments?.getCarbohydrates100g();
-                  // final proteins = product.nutriments?.getProteins100g();
+                  final productName = product.productName;
+                  final ingredientsText = product.ingredientsText;
+                  final nutriments = product.nutriments;
 
                   showDialog(
                     context: context,
@@ -108,12 +83,26 @@ class _ScanCodeState extends State<ScanCode> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisSize: MainAxisSize.min,
                           children: [
-                            // if (energy != null) Text('Energy: $energy kJ'),
-                            // if (fat != null) Text('Fat: $fat g'),
-                            // if (carbohydrates != null)
-                            //   Text('Carbohydrates: $carbohydrates g'),
-                            // if (proteins != null) Text('Proteins: $proteins g'),
-                            if (image != null) Image(image: MemoryImage(image)),
+                            if (ingredientsText != null)
+                              Text('Ingredients: $ingredientsText'),
+                            if (nutriments != null)
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // Text('Nutritional Value:'),
+                                  // if (nutriments.energy_100g != null)
+                                  //   Text('Energy: ${nutriments.energy_100g}'),
+                                  // if (nutriments.fat_100g != null)
+                                  //   Text('Fat: ${nutriments.fat_100g}'),
+                                  // if (nutriments.carbohydrates_100g != null)
+                                  //   Text(
+                                  //       'Carbohydrates: ${nutriments.carbohydrates_100g}'),
+                                  // if (nutriments.proteins_100g != null)
+                                  //   Text('Proteins: ${nutriments.proteins_100g}'),
+                                ],
+                              ),
+                            if (image != null)
+                              Image(image: MemoryImage(image)),
                           ],
                         ),
                       );
@@ -124,7 +113,6 @@ class _ScanCodeState extends State<ScanCode> {
                     Navigator.pop(context);
                   });
                 } else {
-                  // Fallback logic
                   showDialog(
                     context: context,
                     builder: (context) {
@@ -137,8 +125,6 @@ class _ScanCodeState extends State<ScanCode> {
                   );
                 }
               } catch (e) {
-                print('API request error: $e');
-                // Fallback logic
                 showDialog(
                   context: context,
                   builder: (context) {
--- lib/Pages/Search.dart
+++ lib/Pages/Search.dart
@@ -1,61 +1,200 @@
 import 'package:flutter/material.dart';
-import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
+import 'package:test1/Pages/status.dart';
 
+void main() => runApp(const FoodSearch());
 
 class FoodSearch extends StatefulWidget {
-  const FoodSearch({super.key});
+  const FoodSearch({Key? key});
 
   @override
-  State<FoodSearch> createState() => _FoodSearchState();
+  _FoodSearchState createState() => _FoodSearchState();
 }
 
 class _FoodSearchState extends State<FoodSearch> {
+  bool isDark = false;
+  final SearchController _searchController = SearchController();
+
+  Future<void> searchProduct(String query) async {
+    final url = 'https://world.openfoodfacts.org/cgi/search.pl?search_terms=$query&search_simple=1&action=process&json=1';
+    final response = await http.get(Uri.parse(url));
+    
+    if (response.statusCode == 200) {
+      final data = response.body;
+      final products = _parseProducts(data);
+      
+      if (products.isNotEmpty) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Search Results'),
+              content: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+
+                  return ListTile(
+                    title: Text(product['product_name'] ?? 'Unknown'),
+                    subtitle: Text(product['brands'] ?? 'Unknown'),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return FutureBuilder<Map<String, dynamic>>(
+                            future: getProductDetails(product['url']),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return const CircularProgressIndicator();
+                              } else if (snapshot.hasError) {
+                                return const AlertDialog(
+                                  title: Text('Error'),
+                                  content: Text('An error occurred while fetching product details.'),
+                                );
+                              } else {
+                                final details = snapshot.data!;
+
+                                return AlertDialog(
+                                  title: Text(product['product_name'] ?? 'Unknown'),
+                                  content: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      if (details['image_url'] != null)
+                                        Image.network(details['image_url']),
+                                      if (details['ingredients_text'] != null)
+                                        Text('Ingredients: ${details['ingredients_text']}'),
+                                      if (details['nutriments'] != null)
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            const Text('Nutritional Value:'),
+                                            if (details['nutriments']['energy_value'] != null)
+                                              Text('Energy: ${details['nutriments']['energy_value']}'),
+                                            if (details['nutriments']['fat_value'] != null)
+                                              Text('Fat: ${details['nutriments']['fat_value']}'),
+                                            if (details['nutriments']['carbohydrates_value'] != null)
+                                              Text('Carbohydrates: ${details['nutriments']['carbohydrates_value']}'),
+                                            if (details['nutriments']['proteins_value'] != null)
+                                              Text('Proteins: ${details['nutriments']['proteins_value']}'),
+                                          ],
+                                        ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            },
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+            );
+          },
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return const AlertDialog(
+              title: Text('No Results'),
+              content: Text('No products found for the given search query.'),
+            );
+          },
+        );
+      }
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('API Request Error'),
+            content: Text('An error occurred while fetching search results: ${response.statusCode}.'),
+          );
+        },
+      );
+    }
+  }
+
+  List<Map<String, dynamic>> _parseProducts(String data) {
+    final List<Map<String, dynamic>> products = [];
+    final json = parser.parse(data);
+    
+    
+    
+    return products;
+  }
+
+  Future<Map<String, dynamic>> getProductDetails(String url) async {
+    final response = await http.get(Uri.parse(url));
+    
+    if (response.statusCode == 200) {
+      final document = parser.parse(response.body);
+      final imageElement = document.querySelector('.product-summary .js-image-zone img');
+      final ingredientsElement = document.querySelector('.product-summary .js-ingredients-list');
+      final nutrimentsElement = document.querySelector('.product-summary .js-nutriments');
+      
+      final imageUrl = imageElement?.attributes['src'];
+      final ingredients = ingredientsElement?.text;
+      final nutriments = nutrimentsElement?.text;
+      
+      return {
+        'image_url': imageUrl,
+        'ingredients_text': ingredients,
+        'nutriments': nutriments,
+      };
+    } else {
+      throw Exception('An error occurred while fetching product details: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-     OpenFoodAPIConfiguration.userAgent = UserAgent(name: 'Gluten Free Scanner');
-    OpenFoodAPIConfiguration.globalLanguages = <OpenFoodFactsLanguage>[
-      OpenFoodFactsLanguage.ENGLISH
-    ];
-
-    OpenFoodAPIConfiguration.globalCountry = OpenFoodFactsCountry.USA;
-
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: isDark ? Brightness.dark : Brightness.light,
       ),
-      body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SearchAnchor(
-              builder: (BuildContext context, SearchController controller) {
-            return SearchBar(
-              controller: controller,
-              padding: const MaterialStatePropertyAll<EdgeInsets>(
-                  EdgeInsets.symmetric(horizontal: 16.0)),
-              onTap: () {
-                controller.openView();
-              },
-              onChanged: (_) {
-                controller.openView();
-              },
-              leading: const Icon(Icons.search),
-            );
-          }, suggestionsBuilder:
-                  (BuildContext context, SearchController controller) {
-            return List<ListTile>.generate(5, (int index) {
-              final String item = 'item $index';
-              return ListTile(
-                title: Text(item),
-                onTap: () {
-                  setState(() {
-                    controller.closeView(item);
-                  });
-                },
-              );
-            });
-          }),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('OpenFoodFacts Search'),
+          
         ),
-      );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: const InputDecoration(
+                    labelText: 'Search for a product',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  searchProduct(_searchController.text);
+                },
+                child: const Text('Search'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchController extends TextEditingController {
+  @override
+  TextSpan buildTextSpan({required BuildContext context, TextStyle? style, required bool withComposing}) {
+    final textSpan = super.buildTextSpan(context: context, style: style, withComposing: withComposing);
+    return TextSpan(style: style, children: <TextSpan>[textSpan]);
   }
 }
